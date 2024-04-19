d0ed1ff900cca5337f9ea859dc25b43292e257ba
JeoungB <eoehdektnv00@gmail.com>
2024-04-19 12:06:39 +0900

    memo-server

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 const app = express();
-const port = 8080;
+const PORT = 8080;
 
 app.use(express.json()); // json 형식을 해석하기 위해 사용. 안쓰면 req.body = undefined 로 나옴.
 app.use(express.urlencoded({ extended: false })); // url 해석을 위해 사용.
 app.use(express.static(path.join(__dirname + "/public"))); // 백엔드의 정적 파일 경로 지정. 백엔드에 저장되는 이미지의 URL로 접근 가능.
 app.use(cors({ origin: "*" })); // cors 허용. 모든 출저 허용 옵션 : origin.
 
-app.listen(port, function () {
+app.listen(PORT, function () {
   console.log("listening on 8080");
 });
 
@@ -41,5 +41,5 @@ app.post("/img", upload.single("img"), (req, res) => {
 });
 
 app.get('/', function(req, res) {
-   console.log('hello');
+   res.send("hlloe server");
 })
